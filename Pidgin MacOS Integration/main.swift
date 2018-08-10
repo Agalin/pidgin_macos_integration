996a16e44f008c94e5b5fe344db23ed96cf6b711
Agalin <agalin@agalin-mbp-1.home>
2018-08-11 01:47:30 +0200

    Initial Commit

---
+++ Pidgin MacOS Integration/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Pidgin MacOS Integration
+//
+//  Created by Agalin on 11.08.2018.
+//  Copyright © 2018 Agalin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
